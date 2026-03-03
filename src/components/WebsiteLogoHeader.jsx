d4cd4deffc78e9acb5e694326114949bc4ff8c6b
AdityaDeyCode909020 <deyadi710tya777100@gmail.com>
2026-03-03 12:28:41 +0530

    create the website header logo

---
+++ src/components/WebsiteLogoHeader.jsx
@@ -0,0 +1,13 @@
+import './WebsiteLogoHeader.css';
+
+function WebsiteLogoHeader() {
+    return (
+        <header>
+            <div className="logo">
+                <h1>Cue<span className="logo-dot">.</span></h1>
+            </div>
+        </header>
+    );
+}
+
+export default WebsiteLogoHeader;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,13 @@
+import WebsiteLogoHeader from "../components/WebsiteLogoHeader";
+
+function HomePage() {
+    return (
+        <section className="home-page page">
+            <title>Cue</title>
+
+            <WebsiteLogoHeader />
+        </section>
+    );
+}
+
+export default HomePage;
